97c483739e8c317dc8f294060a889a90c3478203
iancov <idcovert23@gmail.com>
2024-11-26 13:59:30 -0500

    added a whole BUNCHA STUFF!

---
+++ main.js
@@ -0,0 +1,54 @@
+function play() {
+    var audio = document.getElementById("anarcofdoves");
+    audio.play();
+  }
+
+function hide() {
+    document.getElementById("click").style.visibility= "hidden";
+    document.getElementById("dovesound").style.visibility= "hidden";
+}
+
+
+function show1() {
+    document.getElementById("page1").style.visibility= "visible";
+}
+
+function hide1() {
+    document.getElementById("page1").style.visibility= "hidden";
+}
+
+
+function show2() {
+    document.getElementById("page2").style.visibility= "visible";
+}
+
+function hide2() {
+    document.getElementById("page2").style.visibility= "hidden";
+}
+
+
+function show3() {
+    document.getElementById("page3").style.visibility= "visible";
+}
+
+function hide3() {
+    document.getElementById("page3").style.visibility= "hidden";
+}
+
+
+function show4() {
+    document.getElementById("page4").style.visibility= "visible";
+}
+
+function hide4() {
+    document.getElementById("page4").style.visibility= "hidden";
+}
+
+
+function show5() {
+    document.getElementById("page5").style.visibility= "visible";
+}
+
+function hide5() {
+    document.getElementById("page5").style.visibility= "hidden";
+}
